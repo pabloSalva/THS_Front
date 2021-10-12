f6620a636eeca331b72d83c4e9bd3f6d2e2199b3
Nicolas Galiñanes <nicolas.galinianes@gmail.com>
2021-10-12 19:26:59 -0300

    se agrego contenido al inicio del sitio

--- src/templates/homeTemplate/index.jsx
+++ src/templates/homeTemplate/index.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 import Layout from "../layout/Layout";
 import { useStyles } from "./styles";
 
 const HomeTemplate = ({ openDrawer }) => {
   const classes = useStyles();
   return (
-    <Layout
-      titulo="Bienvenido/a a Tu Hogar Sustentable THS"
-      openDrawer={openDrawer}
-    ></Layout>
+    <Layout titulo="Bienvenido!" openDrawer={openDrawer}>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/icons/slide.png"}
+            alt="First slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/icons/slide1.jpg"}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/icons/slide2.jpg"}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </Layout>
   );
 };
 
